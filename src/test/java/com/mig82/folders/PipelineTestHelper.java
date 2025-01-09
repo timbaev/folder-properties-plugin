34ca2c1249078bec33dcdb986c64298cd732c850
Timur Shafigullin <hometim55@gmail.com>
2025-01-09 19:32:35 +0300

    Update dependencies

--- src/test/java/com/mig82/folders/PipelineTestHelper.java
+++ src/test/java/com/mig82/folders/PipelineTestHelper.java
@@ -1,6 +1,7 @@
 package com.mig82.folders;
 
 import com.cloudbees.hudson.plugins.folder.Folder;
+import hudson.model.Descriptor;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
@@ -8,7 +9,7 @@ import java.io.IOException;
 
 public class PipelineTestHelper {
 
-	public static WorkflowJob createJob(Folder parent, String name, String script) throws IOException{
+	public static WorkflowJob createJob(Folder parent, String name, String script) throws IOException, Descriptor.FormException {
 		WorkflowJob p = parent.createProject(WorkflowJob.class, name);
 		p.setDefinition(new CpsFlowDefinition(script, true));
 		return p;
